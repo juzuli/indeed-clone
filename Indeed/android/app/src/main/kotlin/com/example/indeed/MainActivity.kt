39e83d556af7441dd7be92a07abb92dcef444eac
Juzuli <juzulitry@gmail.com>
2022-02-14 21:11:26 +0530

    first commit

---
+++ Indeed/android/app/src/main/kotlin/com/example/indeed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.indeed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Indeed/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:indeed/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+
+
